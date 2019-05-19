d36d96520e2675fa342c11a9b8166b02527a1a7b
interceptor128 <polestar.interceptor.0128@gmail.com>
2019-05-19 12:46:07 +0900

    header footer setting

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+		<nav class="footer-nav">
+			<ul>
+				<li><router-link to="/">HOME</router-link></li>
+				<li><router-link to="/about">ABOUT</router-link></li>
+				<li><router-link to="/skill">SKILL</router-link></li>
+				<li><router-link to="/contact">CONTACT</router-link></li>
+			</ul>
+		</nav>
+		<div class="sns-box">
+			<a href="https://twitter.com/interceptor128" target="_blank" class="sns-link">
+				<i class="fab twitter-icon"></i>
+			</a>
+			<a href="https://github.com/interceptor128" target="_blank" class="sns-link">
+				<i class="fab github-icon"></i>
+			</a>
+			<a href="https://instagram.com/interceptor128" target="_blank" class="sns-link">
+				<i class="fab instagram-icon"></i>
+			</a>
+			<a href="https://qiita.com/interceptor128" target="_blank" class="sns-link">
+				<i class="fab qiita-icon"></i>
+			</a>
+			<a href="https://note.mu/interceptor128" target="_blank" class="sns-link">
+				<i class="fab note-icon"></i>
+			</a>
+		</div>
+        <footer>
+            <div class="copyright-box">©️ 2019 Ceptor</div>
+        </footer>
+    </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header-outer">
+        <header>
+            <h1 class="site-title">
+                <router-link to="/">Ceptor's Portfolio</router-link>
+            </h1>
+        </header>
+        <nav id="gnav" class="header-nav">
+            <ul>
+                <li><router-link to="/">HOME</router-link></li>
+                <li><router-link to="/about">ABOUT</router-link></li>
+                <li><router-link to="/skill">SKILL</router-link></li>
+                <li><router-link to="/contact">CONTACT</router-link></li>
+            </ul>
+        </nav>
+    </div>
+</template>
